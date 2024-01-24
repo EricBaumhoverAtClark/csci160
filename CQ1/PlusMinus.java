81817615cba5e85a3367c437a8c95455b445f24e
Eric Baumhover <ericsbaumhover@gmail.com>
2024-01-24 09:53:06 -0500

    Less

--- CQ1/PlusMinus.java
+++ CQ1/PlusMinus.java
@@ -1,62 +1,25 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
-class Result {
-
-    /*
-     * Complete the 'plusMinus' function below.
-     *
-     * The function accepts INTEGER_ARRAY arr as parameter.
-     */
-
-    public static void plusMinus(List<Integer> arr) {
-        double positive = 0;
-        double negative = 0;
-        double zeroes   = 0;
-        
-        for (int val : arr)
-        {
-            if (val > 0)
-            {
-                positive++;
-            }
-            else if (val == 0)
-            {
-                zeroes++;
-            }
-            else
-            {
-                negative++;
-            }
-        }
-        System.out.printf("%.6f\n", positive / arr.size());
-        System.out.printf("%.6f\n", negative / arr.size());
-        System.out.printf("%.6f\n", zeroes   / arr.size());
-    }
-
-}
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
-
-        Result.plusMinus(arr);
-
-        bufferedReader.close();
-    }
+public static void plusMinus(List<Integer> arr) {
+	double positive = 0;
+	double negative = 0;
+	double zeroes   = 0;
+	
+	for (int val : arr)
+	{
+		if (val > 0)
+		{
+			positive++;
+		}
+		else if (val == 0)
+		{
+			zeroes++;
+		}
+		else
+		{
+			negative++;
+		}
+	}
+	System.out.printf("%.6f\n", positive / arr.size());
+	System.out.printf("%.6f\n", negative / arr.size());
+	System.out.printf("%.6f\n", zeroes   / arr.size());
 }
